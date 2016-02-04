06bfb2a06591e4a5fcd7b22d73ef226d60904f0a
Mies <miesja.yogo@gmail.com>
2016-02-04 19:56:20 +0100

    bananen zijn geel krom en op dezelfde lijn

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("let's escape our 'unsolvable' problem!!! HELLO" );
-        System.out.println("bananen zijn geel en krom" );
+    System.out.println("bananen zijn geel en krom" );
     }
 }
