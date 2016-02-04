3a36100714faac9fc86f1efe36bd59135262a1b9
Bas <bas@dehoogen.nl>
2016-02-04 19:54:39 +0100

    Yeey duimen dat het werkt...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("let's escape our 'unsolvable' problem!!! HELLO" );
+        System.out.println("bananen zijn geel en krom" );
     }
 }
