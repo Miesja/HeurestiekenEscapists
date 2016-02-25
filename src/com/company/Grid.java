0d817bc2a15cdf8707192252550bb5fd3d0a27c7
Bas <bas@dehoogen.nl>
2016-02-25 18:22:29 +0100

    Beetje proberen een losse class te maken voor het grid/veld

---
+++ src/com/company/Grid.java
@@ -0,0 +1,32 @@
+package com.company;
+
+/**
+ * Created by Bas on 25-2-2016.
+ */
+public class Grid {
+
+    /*
+    int lengte;
+    int breedte;
+
+    public Grid ( int lengte, int breedte)
+    {
+        lengte = this.lengte;
+        breedte = this.breedte;
+    }
+
+    public void maakVeld()
+    {
+        for( int i = 0; i < lengte; i++ )
+        {
+            String Row = "";
+            for( int j = 0; j < breedte; j++)
+            {
+                String A = veld.Field[ i ][ j ];
+                Row += A;
+            }
+            System.out.println( Row );
+        }
+    }
+     */
+}
