9ee6eb4a1122342b1be1d64ca12d57b4ecde17ac
Mies <miesja.yogo@gmail.com>
2016-02-25 18:12:31 +0100

    nog meer tilestuff

--- src/com/company/Tile.java
+++ src/com/company/Tile.java
@@ -22,3 +22,48 @@ package com.company;
         return "hello";
     }
 }*/
+
+    public class Tile{
+
+        int width;
+        int length;
+        String name;
+
+        public Tile(int width, int length, String name){
+            width = width;
+            this.width = width;
+            length = length;
+            this.length = length;
+            name = "";
+            this.name = name;
+            return width;
+            return length;
+            return name;
+
+        }
+
+
+        private void makeTile() {
+            for (int i = 0; i < 3; i++) {
+               Tile tile+i = new Tile (7, 7, name);
+                    name = "7T" + i;
+            }
+
+            for (int j = 0; j < 3; j++) {
+                Tile tile+j = new Tile(5, 5, "5T" + j);
+            }
+            for (int k = 8; k < 13; k++) {
+                Tile tile+k = new Tile(7, 7, "3T" + k);
+            }
+            Tile tile2 = new Tile(2, 2, "2T");
+        }
+    }
+
+
+ptgenerate (i)
+
+
+
+
+
+
