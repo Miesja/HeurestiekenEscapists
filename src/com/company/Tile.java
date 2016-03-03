5ed81093a37b9b4d0d73d0d8e22c9ca578cbfbe8
Bas <bas@dehoogen.nl>
2016-03-03 17:20:26 +0100

    bunaan

--- src/com/company/Tile.java
+++ src/com/company/Tile.java
@@ -10,7 +10,7 @@ public class Tile {
     int length;
     String name;
     // de constructor om een type:Tile te maken met een bepaalde breedte (width), lengte (length) en naam in het veld.
-    // tevens voegt het de breedte van de gemaakte tegel toe aan een array (tileWidths) om daar combi's mee te maken.
+    // tevens voegt het de breedte van de gemaakte tegel toe aan een array (tileWidths) om daar combinaties mee te maken.
     public Tile(int width, int length, String name) {
         this.width = width;
         this.length = length;
