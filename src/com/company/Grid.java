dcab36d1b4256fbbd4609db3cb00512456b6c483
Bas <bas@dehoogen.nl>
2016-03-03 19:22:30 +0100

    controleerPLaats fix

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -59,17 +59,17 @@ public class Grid {
 
     // controleert of er geen tegel ligt waar de nieuwe tegel geplaatst wilt worden
     public boolean controleerPlaats(Tile tegel, int XPoint, int YPoint){
+        boolean vrij;
         for (int x = XPoint-1; x < (XPoint-1 + tegel.width); x++) {
             for (int y = YPoint - 1; y < YPoint - 1 + tegel.length; y++) {
                 String s = field[x][y];
-                boolean vrij;
                 if (s.equals(" 0 ")) {
                     vrij = true;
                 } else {
                     vrij = false;
                 }
-                return vrij;
             }
         }
+        return vrij;
     }
 }
