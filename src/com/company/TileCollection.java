674d2f64c0a9c967545897f032d0c4c9a85adc1d
Bas <bas@dehoogen.nl>
2016-03-17 19:29:53 +0100

    update aan TileCollection

--- src/com/company/TileCollection.java
+++ src/com/company/TileCollection.java
@@ -9,12 +9,12 @@ public class TileCollection {
     //Haalt een specifieke tile uit de collectie en geeft deze terug zodat er verder mee gewerkt kan worden
     public Tile giveTile(int index){
         Tile x = tiles.get(index);
-        removeTileFromCollection(index);
+        //remove( tile);
         return x;
     }
 
     //Haalt een tile uit de collectie zodat deze niet nogmaals gebruikt kan worden in een Grid
-    public void removeTileFromCollection(int index){
-        tiles.remove(index);
+    public void remove(Tile tile){
+        tiles.remove(tile);
     }
 }
