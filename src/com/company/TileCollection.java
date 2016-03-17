e41918c53bb7c4e0a944c262b252708f61ffd9d2
Bas <bas@dehoogen.nl>
2016-03-17 19:26:52 +0100

    update aan TileCollection

--- src/com/company/TileCollection.java
+++ src/com/company/TileCollection.java
@@ -5,4 +5,16 @@ import java.util.ArrayList;
 
 public class TileCollection {
     ArrayList<Tile> tiles = new ArrayList<>();
+
+    //Haalt een specifieke tile uit de collectie en geeft deze terug zodat er verder mee gewerkt kan worden
+    public Tile giveTile(int index){
+        Tile x = tiles.get(index);
+        removeTileFromCollection(index);
+        return x;
+    }
+
+    //Haalt een tile uit de collectie zodat deze niet nogmaals gebruikt kan worden in een Grid
+    public void removeTileFromCollection(int index){
+        tiles.remove(index);
+    }
 }
