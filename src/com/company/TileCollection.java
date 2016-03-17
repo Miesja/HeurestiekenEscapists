8ed6481baf3f021c0dc16e6e0606b84520b48d71
ManSchri <manon.schriever@student.uva.nl>
2016-03-17 19:30:53 +0100

    x

--- src/com/company/TileCollection.java
+++ src/com/company/TileCollection.java
@@ -9,7 +9,7 @@ public class TileCollection {
     //Haalt een specifieke tile uit de collectie en geeft deze terug zodat er verder mee gewerkt kan worden
     public Tile giveTile(int index){
         Tile x = tiles.get(index);
-        //remove( tile);
+        remove(x);
         return x;
     }
 
