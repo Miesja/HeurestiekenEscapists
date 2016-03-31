0490b9f1d9f83426ff99753cdebb276744f4d728
Bas <bas@dehoogen.nl>
2016-03-31 19:21:36 +0200

    Class GI gemaakt

---
+++ src/com/company/GI.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by Bas on 31-3-2016.
+ */
+
+import java.awt.*;
+import java.awt.Graphics;
+
+//Deze klasse verzorgt de Grafische Weergave (GI) van het probleem
+//Het grid wordt hier ipv gevuld met letters, daadwerkelijk gevuld met tegels
+public class GI {
+
+    public void makeCanvas(){
+        new Canvas();
+    }
+
+    //Maakt de tegels en plaatst ze op het canvas
+    public void makeTiles(int x, int y, int width, int length){
+        Point p = new Point(x*10, y*10);
+        Dimension d= new Dimension(width*10, length*10);
+        Rectangle tile= new Rectangle(p, d);
+    }
+}
