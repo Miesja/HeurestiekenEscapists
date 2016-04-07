f93ffd0daef44e61bc482f6c2d8c404d786c5f84
Bas <bas@dehoogen.nl>
2016-04-07 20:03:36 +0200

    class Tile en class TileCollection maximale regel lengte naar 80 terug gebracht

--- src/com/company/TileCollection.java
+++ src/com/company/TileCollection.java
@@ -6,18 +6,21 @@ import java.util.ArrayList;
 public class TileCollection {
     ArrayList<Tile> tiles = new ArrayList<>();
 
-    //Haalt een specifieke tile uit de collectie en geeft deze terug zodat er verder mee gewerkt kan worden
+    //Haalt een specifieke tile uit de collectie en geeft deze terug zodat er
+    // verder mee gewerkt kan worden
     public Tile giveTile(int index){
         Tile x = tiles.get(index);
         return x;
     }
 
-    //Haalt een tile uit de collectie zodat deze niet nogmaals gebruikt kan worden in een Grid
+    //Haalt een tile uit de collectie zodat deze niet nogmaals gebruikt kan
+    //worden in een Grid
     public void removeTile(Tile tile){
         tiles.remove(tile);
     }
 
-    //deze functie draait een tegel op zijn zij, dit gebeurd door lengte en breedte om te draaien
+    //deze functie draait een tegel op zijn zij, dit gebeurd door lengte
+    //en breedte om te draaien
     public Tile rotateTile(int index){
         int x;
         Tile tile = tiles.get(index);
