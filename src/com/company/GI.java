4b88c8d797be946acd962e2eb843aa606a67abfd
ManSchri <manon.schriever@student.uva.nl>
2016-05-23 19:46:17 +0200

    Meer kleuren toegevoegd

--- src/com/company/GI.java
+++ src/com/company/GI.java
@@ -29,86 +29,95 @@ package com.company;
 
                         if (GI.field[i][j].equals(" a ")) {
                             g2d.setColor(new Color(100, 40, 100));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" b ")) {
                             g2d.setColor(new Color(78, 100, 180));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" c ")) {
                             g2d.setColor(new Color(50, 200, 100));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" d ")) {
                             g2d.setColor(new Color(200, 200, 100));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" e ")) {
                             g2d.setColor(new Color(100, 200, 200));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" f ")) {
                             g2d.setColor(new Color(150, 230, 100));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" g ")) {
                             g2d.setColor(new Color(50, 10, 210));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" h ")) {
                             g2d.setColor(new Color(200, 10, 200));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" i ")) {
                             g2d.setColor(new Color(200, 100, 10));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" j ")) {
                             g2d.setColor(new Color(50, 50, 60));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" k ")) {
                             g2d.setColor(new Color(200, 200, 200));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" l ")) {
                             g2d.setColor(new Color(100, 100, 150));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" m ")) {
                             g2d.setColor(new Color(0, 200, 250));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" n ")) {
                             g2d.setColor(new Color(100, 10, 200));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" o ")) {
                             g2d.setColor(new Color(200, 60, 160));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" p ")) {
                             g2d.setColor(new Color(200, 110, 40));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" q ")) {
                             g2d.setColor(new Color(30, 40, 100));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" r ")) {
                             g2d.setColor(new Color(10, 200, 10));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" s ")) {
                             g2d.setColor(new Color(170, 90, 150));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
                         if (GI.field[i][j].equals(" t ")) {
                             g2d.setColor(new Color(250, 5, 80));
-                            g2d.fillRect(x, y, tileSize, tileSize);
                         }
-
-
+                        if (GI.field[i][j].equals(" u ")) {
+                            g2d.setColor(new Color(102, 255, 255));
+                        }
+                        if (GI.field[i][j].equals(" v ")) {
+                            g2d.setColor(new Color(255, 204, 229));
+                        }
+                        if (GI.field[i][j].equals(" w ")) {
+                            g2d.setColor(new Color(255, 255, 204));
+                        }
+                        if (GI.field[i][j].equals(" x ")) {
+                            g2d.setColor(new Color(204, 255, 229));
+                        }
+                        if (GI.field[i][j].equals(" y ")) {
+                            g2d.setColor(new Color(102, 0, 51));
+                        }
+                        if (GI.field[i][j].equals(" z ")) {
+                            g2d.setColor(new Color(255, 255, 0));
+                        }
+                        if (GI.field[i][j].equals(" A ")) {
+                            g2d.setColor(new Color(51, 0, 102));
+                        }
+                        if (GI.field[i][j].equals(" B ")) {
+                            g2d.setColor(new Color(200, 100, 100));
+                        }
+                        if (GI.field[i][j].equals(" C ")) {
+                            g2d.setColor(new Color(178, 102, 255));
+                        }
+                        if (GI.field[i][j].equals(" D ")) {
+                            g2d.setColor(new Color(255, 255, 255));
+                        }
+                        g2d.fillRect(x, y, tileSize, tileSize);
 
                     }
                 }
